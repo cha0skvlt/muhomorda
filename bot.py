7d68ee3321dfb375c4872e3a46ee9d78ce9ea807
Evgeniy Tomashkov <cha0skvlt@gmail.com>
2025-05-07 09:23:23 +0300

    2.

--- bot.py
+++ bot.py
@@ -5,17 +5,19 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 from dotenv import load_dotenv
 import telebot
 from postgen import generate_muhomor_post
+from db import is_duplicate_post
 
 # ───── Load config
 load_dotenv()
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 CHANNEL_ID = os.getenv("TELEGRAM_CHANNEL_ID")
 
-bot = telebot.TeleBot(TELEGRAM_TOKEN)
+bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
 
-# ───── Run once
 def post_to_channel():
     post = generate_muhomor_post()
+    if is_duplicate_post(post["content"]):
+        return
     bot.send_message(CHANNEL_ID, post["content"], parse_mode="Markdown")
 
 if __name__ == "__main__":
